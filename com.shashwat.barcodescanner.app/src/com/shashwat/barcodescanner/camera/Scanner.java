061781caff8235df7de7d11735281992629ccc11
Shashwat Anand <shashwat.anand@gmail.com>
2016-09-26 22:00:03 +0530

    Added barcode reading hint

--- com.shashwat.barcodescanner.app/src/com/shashwat/barcodescanner/camera/Scanner.java
+++ com.shashwat.barcodescanner.app/src/com/shashwat/barcodescanner/camera/Scanner.java
@@ -1,8 +1,12 @@
 package com.shashwat.barcodescanner.camera;
 
 import java.awt.image.BufferedImage;
+import java.util.HashMap;
+import java.util.Map;
 
+import com.google.zxing.BarcodeFormat;
 import com.google.zxing.BinaryBitmap;
+import com.google.zxing.DecodeHintType;
 import com.google.zxing.LuminanceSource;
 import com.google.zxing.MultiFormatReader;
 import com.google.zxing.Result;
@@ -33,11 +37,26 @@ public class Scanner implements Runnable {
 				}
 				LuminanceSource  luminanceSource  = new BufferedImageLuminanceSource(image);
 				BinaryBitmap source = new BinaryBitmap(new GlobalHistogramBinarizer(luminanceSource));
-				Result decode = new MultiFormatReader().decode(source);
-				listener.resolved(new ScanResult(decode.getText()));
+				MultiFormatReader reader = new MultiFormatReader();
+				Map<DecodeHintType, Object> hints = new HashMap<DecodeHintType, Object>();
+		        hints.put(DecodeHintType.TRY_HARDER, BarcodeFormat.CODE_39);
+		        reader.setHints(hints);
+				Result decode = reader.decodeWithState(source);
+				BarcodeFormat barcodeFormat = decode.getBarcodeFormat();
+				if(barcodeFormat.compareTo(BarcodeFormat.QR_CODE) != 0) {
+					System.out.println("Barcode found : " + decode.getText());
+				} else {					
+					listener.resolved(new ScanResult(decode.getText()));
+				}
+				flag = true;
 				break;
 			} catch (Exception e) {
 				e.printStackTrace();
+				try {
+					Thread.sleep(2000);
+				} catch (InterruptedException e1) {
+					e1.printStackTrace();
+				}
 			}
 		} while (!flag);
 	}

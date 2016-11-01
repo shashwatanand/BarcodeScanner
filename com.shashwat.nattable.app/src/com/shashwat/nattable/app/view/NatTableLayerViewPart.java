1f4f130cff2729e7c03c42d953ccef805ba090cf
Shashwat Anand <shashwat.anand@gmail.com>
2016-11-01 10:28:07 +0530

    Updated

--- com.shashwat.nattable.app/src/com/shashwat/nattable/app/view/NatTableLayerViewPart.java
+++ com.shashwat.nattable.app/src/com/shashwat/nattable/app/view/NatTableLayerViewPart.java
@@ -1,13 +1,41 @@
- 
+
 package com.shashwat.nattable.app.view;
 
 import javax.annotation.PostConstruct;
 
+import org.eclipse.jface.layout.GridDataFactory;
+import org.eclipse.nebula.widgets.nattable.NatTable;
+import org.eclipse.nebula.widgets.nattable.data.IColumnPropertyAccessor;
+import org.eclipse.nebula.widgets.nattable.data.IDataProvider;
+import org.eclipse.nebula.widgets.nattable.data.ListDataProvider;
+import org.eclipse.nebula.widgets.nattable.data.ReflectiveColumnPropertyAccessor;
+import org.eclipse.nebula.widgets.nattable.grid.GridRegion;
+import org.eclipse.nebula.widgets.nattable.layer.DataLayer;
+import org.eclipse.nebula.widgets.nattable.selection.SelectionLayer;
+import org.eclipse.nebula.widgets.nattable.viewport.ViewportLayer;
+import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
+
+import com.shashwat.nattable.app.model.Person;
+import com.shashwat.nattable.app.model.Service;
 
 public class NatTableLayerViewPart {
 	@PostConstruct
 	public void postConstruct(Composite parent) {
+		parent.setLayout(new GridLayout());
+		String[] propertyNames = { "firstName", "lastName", "gender", "married", "birthday" };
+
+		// data provider
+		IColumnPropertyAccessor<Person> columnPropertyAccessor = new ReflectiveColumnPropertyAccessor<>(propertyNames);
+		IDataProvider dataProvider = new ListDataProvider<>(Service.getPersons(50), columnPropertyAccessor);
 		
+		// Layer
+		DataLayer dataLayer = new DataLayer(dataProvider);
+		SelectionLayer selectionLayer = new SelectionLayer(dataLayer);
+		ViewportLayer viewportLayer = new ViewportLayer(selectionLayer);
+		viewportLayer.setRegionName(GridRegion.BODY);
+		
+		NatTable natTable = new NatTable(parent, viewportLayer);
+		GridDataFactory.fillDefaults().grab(true, true).applyTo(natTable);
 	}
 }

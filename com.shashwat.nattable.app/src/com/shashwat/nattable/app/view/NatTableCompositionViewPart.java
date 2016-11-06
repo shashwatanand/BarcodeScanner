4c68e77c0232a63856dc89c36e792f0338c2c1e8
Shashwat Anand <shashwat.anand@gmail.com>
2016-11-06 14:36:51 +0530

    Update

--- com.shashwat.nattable.app/src/com/shashwat/nattable/app/view/NatTableCompositionViewPart.java
+++ com.shashwat.nattable.app/src/com/shashwat/nattable/app/view/NatTableCompositionViewPart.java
@@ -1,13 +1,26 @@
  
 package com.shashwat.nattable.app.view;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.annotation.PostConstruct;
 
+import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 
 public class NatTableCompositionViewPart {
 	@PostConstruct
 	public void postConstruct(Composite parent) {
+		parent.setLayout(new GridLayout());
+		String[] propertyNames = {"firstName", "lastName", "gender", "married", "birthday"};
 		
+		// mapping from property to label, needed for column header labels
+	    Map<String, String> propertyToHeaderMap = new HashMap<String, String>();
+	    propertyToHeaderMap.put("firstName", "Firstname");
+	    propertyToHeaderMap.put("lastName", "Lastname");
+	    propertyToHeaderMap.put("gender", "Gender");
+	    propertyToHeaderMap.put("married", "Married");
+	    propertyToHeaderMap.put("birthday", "Birthday");
 	}
 }

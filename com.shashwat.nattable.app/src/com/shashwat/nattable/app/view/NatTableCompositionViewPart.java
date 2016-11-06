4943d8d346d4202022bd4dad272830b49f704f3f
Shashwat Anand <shashwat.anand@gmail.com>
2016-11-06 14:35:11 +0530

    Updating the nat table class, removing unused class

--- com.shashwat.nattable.app/src/com/shashwat/nattable/app/view/NatTableCompositionViewPart.java
+++ com.shashwat.nattable.app/src/com/shashwat/nattable/app/view/NatTableCompositionViewPart.java
@@ -1,22 +1,13 @@
  
 package com.shashwat.nattable.app.view;
 
-import javax.inject.Inject;
 import javax.annotation.PostConstruct;
+
 import org.eclipse.swt.widgets.Composite;
 
 public class NatTableCompositionViewPart {
-	@Inject
-	public NatTableCompositionViewPart() {
-		
-	}
-	
 	@PostConstruct
 	public void postConstruct(Composite parent) {
 		
 	}
-	
-	
-	
-	
 }
